cc36787e00b0d1ef868500ec6d1e2d73dfdbfd53
Carlos Ortega <coforfe@users.noreply.github.com>
2022-06-22 11:21:35 +0200

    Comment not needed code. Clean variable names and save file.

--- 00_Comercial_Distrito_.R
+++ 00_Comercial_Distrito_.R
@@ -66,36 +66,36 @@ dele_dt %<>%
   mutate.( localidad = stri_replace_all_fixed(localidad, "L’", "")) %>% 
   mutate.( localidad = stri_replace_all_fixed(localidad, " HOSPITALET", "HOSPITALET")) %>% 
   as.data.table()
-table(dele_dt$localidad)
+# table(dele_dt$localidad)
 
 #--- Select just the commercial people and the Staffing as business
 # com_god <- c('RESPONSABLE COMERCIAL', 'RESPONSABLE GESTIÓN', 'DIRECTOR')
 dele_com <- dele_dt %>%
-  filter.(puesto %like% "COMERCIAL" | puesto %like% "DIRECTOR") %>%
-  filter.(!(puesto %like% "TERRITORIAL" | puesto %like% "REGIONAL" | puesto %like% "DE DELEGACIÓN")) %>%
-  filter.(negocio == "STAFFING") %>%
+  filter.( puesto %like% "COMERCIAL" | puesto %like% "DIRECTOR" | puesto %like% "DISTRICT" ) %>%
+  filter.( !(puesto %like% "TERRITORIAL" | puesto %like% "REGIONAL" | puesto %like% "DE DELEGACIÓN") ) %>%
+  filter.( negocio == "STAFFING" ) %>%
   as.data.table()
-table(dele_com$puesto)
-table(dele_dt$puesto)
+# table(dele_com$puesto)
+# table(dele_dt$puesto)
 #-- (END) --- Staffing Structure.
 
 
-#----- LOCALIDAD AND POSTAL CODE - github
-#-- In this file we have all postal codes avaialbles by location (nombre)
-postal_dir  <- '/Users/carlosortega/Documents/00_Adecco/Para_J/01_Input_raw/CodigosPostales_Localidades/' 
-file_postal <- 'codigos_postales_municipios_join.csv'
-filedirpostal <- paste(postal_dir,file_postal, sep = "")
-postallocal <- fread(filedirpostal)
+# #----- LOCALIDAD AND POSTAL CODE - github
+# #-- In this file we have all postal codes avaialbles by location (nombre)
+# postal_dir  <- '/Users/carlosortega/Documents/00_Adecco/Para_J/01_Input_raw/CodigosPostales_Localidades/' 
+# file_postal <- 'codigos_postales_municipios_join.csv'
+# filedirpostal <- paste(postal_dir,file_postal, sep = "")
+# postallocal <- fread(filedirpostal)
 
 
-#----- LOCALIDAD AND POSTAL CODE - Maestro
-#-- Here we have all the "localidad" related to DR. but there many missing CPs. 
-maestro_dir  <- '/Users/carlosortega/Documents/00_Adecco/Para_J/01_Input_raw/Relacion_CCAA_Provincia_Municipios/' 
-file_maestr  <- 'CCAA_Provincia_Municipios.xlsx'
-filedirmaes  <- paste(maestro_dir, file_maestr, sep = "")
-maestrolocal <- read_excel(path = filedirmaes) %>% 
-  clean_names() %>%
-  as.data.table()
+# #----- LOCALIDAD AND POSTAL CODE - Maestro
+# #-- Here we have all the "localidad" related to DR. but there many missing CPs. 
+# maestro_dir  <- '/Users/carlosortega/Documents/00_Adecco/Para_J/01_Input_raw/Relacion_CCAA_Provincia_Municipios/' 
+# file_maestr  <- 'CCAA_Provincia_Municipios.xlsx'
+# filedirmaes  <- paste(maestro_dir, file_maestr, sep = "")
+# maestrolocal <- read_excel(path = filedirmaes) %>% 
+#   clean_names() %>%
+#   as.data.table()
 
 # #----- FTEs
 # #-- FTEs is not needed, since we have everything in dele_dt
@@ -146,37 +146,92 @@ dat2021red <- dat2021 %>%
                           ) %>%
                mutate.( provincia = stri_replace_all_fixed(
                                     provincia,
-                                    c("LA COURUÑA", "VIZCAYA", "ALMERIA", "LEON", "CADIZ",  "CACERES", "GUIPUZCOA", "LERIDA"),
-                                    c("A CORUÑA", "BIZCAIA" , "ALMERÍA", "LEÓN", "CÁDIZ",  "CÁCERES", "GIPUZKOA", "LLEIDA"),
+                                    c("LA CORUÑA", "VIZCAYA", "ALMERIA", "LEON", "CADIZ",  "CACERES", 
+                                      "GUIPUZCOA", "LERIDA", "AVILA", "CASTELLON", "BALEARES", "MALAGA"),
+                                    c("A CORUÑA", "BIZKAIA" , "ALMERÍA", "LEÓN", "CÁDIZ",  "CÁCERES", 
+                                      "GIPUZKOA", "LLEIDA", "ÁVILA", "CASTELLÓN", "PALMA DE MALLORCA", "MÁLAGA"),
                                     vectorize_all = FALSE
                                     )
                ) %>%
                as.data.table()
 toc(func.toc = toc.outmsg)
-rm(dat2021)
+# rm(dat2021)
 #---------- END OF FILE ----------------
 
-#--- Case of Asturias...
-duns_asturias <- dat2021red %>%
-  filter.(provincia == "ASTURIAS") %>%
-  as.data.table()
+# #--- Case of Asturias...
+# duns_asturias <- dat2021red %>%
+#   filter.(provincia == "ASTURIAS") %>%
+#   as.data.table()
+# 
+# dele_asturias <- dele_com %>%
+#   filter.(provincia == "ASTURIAS") %>%
+#   mutate.(nomape = paste(nombre, apellidos, sep = "_")) %>%
+#   as.data.table()
+# 
+# duns_asturias %<>%
+#   mutate.(nom_ape = rep(dele_asturias$nomape, length.out = nrow(duns_asturias)) ) %>%
+#   separate.(nom_ape, into = c('nombre', 'apellidos'), sep = "_") %>%
+#   left_join.(dele_asturias, by = c("nombre" = "nombre", "apellidos" = "apellidos") ) %>%
+#   as.data.table()
 
-dele_asturias <- dele_com %>%
-  filter.(provincia == "ASTURIAS") %>%
-  mutate.(nomape = paste(nombre, apellidos, sep = "_")) %>%
-  as.data.table()
-
-duns_asturias %<>%
-  mutate.(nom_ape = rep(dele_asturias$nomape, length.out = nrow(duns_asturias)) ) %>%
-  separate.(nom_ape, into = c('nombre', 'apellidos'), sep = "_") %>%
-  left_join.(dele_asturias, by = c("nombre" = "nombre", "apellidos" = "apellidos") ) %>%
-  as.data.table()
-
+#--------- BUILD THE COMMERCIAL - DISTRICT DATAFRAME ------
 #-- Run for all provinces except MADRID and BARCELONA
-prov_val <- dat2021red %>%
-            select.(provincia) %>%
-            filter.(provincia != "") %>%
+prov_val <- dele_com %>%
+            select.( provincia) %>%
+            filter.( provincia != "") %>%
+            filter.( provincia != "ANDORRA") %>%
+            filter.( provincia != "LANZAROTE") %>%
             distinct.() %>%
             pull.(provincia)
-          
 
+salesdistrict <- data.table()
+for (i in 1:length(prov_val)) {
+  
+  prov_tmp <- prov_val[i] 
+  print(i)
+  
+  duns_df  <- dat2021red %>%
+    filter.( provincia == prov_tmp ) %>%
+    as.data.table()
+  
+  #-- This case when name of province does not match what is in Duns.
+  print(prov_tmp)
+  if (nrow(duns_df) == 0) { 
+       print(i)
+       break 
+    }
+  
+  dele_df  <- dele_com %>%
+    filter.( provincia == prov_tmp ) %>%
+    mutate.( nomape = paste(nombre, apellidos, sep = "_") ) %>%
+    as.data.table()
+  
+  duns_df %<>%
+    mutate.( nom_ape = rep(dele_df$nomape, length.out = nrow(duns_df)) ) %>%
+    separate.( nom_ape, into = c('nombre', 'apellidos'), sep = "_") %>%
+    left_join.( dele_df, by = c("nombre" = "nombre", "apellidos" = "apellidos") ) %>%
+    as.data.table() 
+  
+  salesdistrict <- rbind(salesdistrict, duns_df) 
+  
+} #for (i in 
+          
+#--- Clean final file...
+salesdistrict_gd <- salesdistrict %>%
+  select.(-nomape, -provincia.y) %>%
+  rename.(localidad_duns = localidad.x) %>%
+  rename.(localidad_delegacion = localidad.y) %>%
+  rename.(provincia = provincia.x) %>%
+  as.data.table()
+  
+#--- Save file 
+fwrite(
+       salesdistrict_gd,
+       file = "./output/Comercial_Distrito_.csv",
+       sep = "|",
+       encoding = "UTF-8"
+)
+
+
+tini <- Sys.time()
+#----------- END OF FILE ---------
